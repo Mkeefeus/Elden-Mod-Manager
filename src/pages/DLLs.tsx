7a4c666856517b891f21c03e96d8d9b10043cf4c
Malcolm <27903055+Mkeefeus@users.noreply.github.com>
2023-09-05 12:09:13 -0400

    Init, navbar

---
+++ src/pages/DLLs.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DLLs = () => {
+  return (
+    <div>DLLs</div>
+  )
+}
+
+export default DLLs
---
+++ src/pages/Mods.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mods = () => {
+  return (
+    <div>Mods</div>
+  )
+}
+
+export default Mods
