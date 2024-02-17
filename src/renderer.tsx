baf44b26b301842d9846358270ba125fcb98852c
Mkeefeus <Mkeefeus@gmail.com>
2024-02-16 20:29:48 -0500

    inital react setup

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -26,6 +26,14 @@
  * ```
  */
 
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
