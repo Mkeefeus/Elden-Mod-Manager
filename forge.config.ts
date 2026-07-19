0e1d657c2c4c03c6c01c0d6b474cdfe533aac00f
Malcolm Keefe <Malcolm.keefe@proton.me>
2026-07-18 21:50:40 -0400

    fix nupkg naming

--- forge.config.ts
+++ forge.config.ts
@@ -153,7 +153,7 @@ const config: ForgeConfig = {
           }
 
           if (currentBaseName === 'RELEASES' || extension === 'nupkg') {
-            nextName = `${currentBaseName}${detectedExt ? `.${detectedExt}` : ''}`;
+            nextName = `${currentBaseName}`;
           }
 
           usedNames.add(nextName);
